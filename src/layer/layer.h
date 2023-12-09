a34163e5f7227e8735d184656d91a0d00f4c61bc
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-09 18:09:49 +0800

    feat(layer.h): add activation_function member

--- src/layer/layer.h
+++ src/layer/layer.h
@@ -9,12 +9,14 @@ class Layer
 private:
     size_t _n_in;
     size_t _n_out;
+    std::string _activate_function = "tanh";
     std::vector<Neuron> _neurons;
 
 public:
-    Layer(size_t n_in, size_t n_out)
-        : _n_in(n_in), _n_out(n_out),
-          _neurons(std::vector<Neuron>(n_out, Neuron(n_in))){};
+    Layer(size_t n_in, size_t n_out, std::string activate_function = "tanh")
+        : _n_in(n_in), _n_out(n_out), _activate_function(activate_function),
+          _neurons(
+              std::vector<Neuron>(n_out, Neuron(n_in, activate_function))){};
 
     std::vector<Variable> parameters() const;
 
