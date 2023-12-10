7f8c49574bdeab6d79e3baa946c6882ffb84b876
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-10 17:07:25 +0800

    feat(mlp): add multiple layer perception

--- src/neural_network/mlp.cc
+++ src/neural_network/mlp.cc
@@ -1,1 +1,24 @@
 #include "mlp.h"
+
+
+std::vector<Variable> MLP::parameters() const
+{
+    std::vector<Variable> result;
+    for (const auto &layer : _layers)
+    {
+        result.insert(result.end(),
+                      layer.parameters().begin(),
+                      layer.parameters().end());
+    }
+    return result;
+}
+
+std::vector<Variable> MLP::forward(const std::vector<double> &inputs)
+{
+    std::vector<Variable> result = _layers[0].forward(inputs);
+    for (size_t i = 1; i < _layers.size(); i++)
+    {
+        result = _layers[i].forward(value(result));
+    }
+    return result;
+}
--- src/neural_network/mlp.h
+++ src/neural_network/mlp.h
@@ -6,4 +6,23 @@
 
 class MLP
 {
+private:
+    size_t _n_in;
+    std::vector<size_t> _n_outs;
+    std::vector<Layer> _layers;
+
+public:
+    MLP(size_t n_in, std::vector<size_t> n_outs) : _n_in(n_in), _n_outs(n_outs)
+    {
+        size_t n_prev = n_in;
+        for (size_t n_out : n_outs)
+        {
+            _layers.push_back(Layer(n_prev, n_out));
+            n_prev = n_out;
+        }
+    }
+
+    std::vector<Variable> parameters() const;
+
+    std::vector<Variable> forward(const std::vector<double> &inputs);
 };
