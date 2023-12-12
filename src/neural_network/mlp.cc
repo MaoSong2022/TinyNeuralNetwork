51eeea025c7921eaa65090429e59531b7d4736b2
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-12 22:30:43 +0800

    fix(mlp.cc): forward process error

--- src/neural_network/mlp.cc
+++ src/neural_network/mlp.cc
@@ -18,7 +18,7 @@ std::vector<Variable> MLP::forward(const std::vector<double> &inputs)
     std::vector<Variable> result = _layers[0].forward(inputs);
     for (size_t i = 1; i < _layers.size(); i++)
     {
-        result = _layers[i].forward(value(result));
+        result = _layers[i].forward(result);
     }
     return result;
 }
