2cf2f848ee2b44035a6157e11d5616352c07166f
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-12 22:28:20 +0800

    refactor(loss.cc): simplify total operations

--- src/loss/loss.cc
+++ src/loss/loss.cc
@@ -4,11 +4,26 @@
 Variable MSELoss(std::vector<Variable> predictions, std::vector<double> targets)
 {
     size_t n = predictions.size();
-    Variable sum;
+    double value = 0;
     for (size_t i = 0; i < n; i++)
     {
-        sum =
-            sum + (predictions[i] - targets[i]) * (predictions[i] - targets[i]);
+        value += (predictions[i].value() - targets[i]) *
+                 (predictions[i].value() - targets[i]);
     }
-    return Variable(sum) / static_cast<double>(n);
+    value /= static_cast<double>(n);
+
+    Variable result;
+    result.set_value(value);
+    result.set_children(predictions);
+    result.set_op("MSELoss");
+    result.set_backward([targets](Variable *result) {
+        for (size_t i = 0; i < result->children().size(); i++)
+        {
+            Variable &child = result->mutable_children()[i];
+            child.update_gradient(result->gradient() * 2.0 *
+                                  (child.value() - targets[i]));
+        }
+    });
+
+    return result;
 }
