c670530fb9932698655d3df17592b5e512ebe757
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-12 22:27:16 +0800

    refactor(neuron.h): use random number to initialize weights

--- src/neuron/neuron.h
+++ src/neuron/neuron.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <chrono>
+#include <random>
 #include <vector>
 
 #include "../variable/variable.h"
@@ -13,8 +15,23 @@ private:
 
 public:
     Neuron(size_t n_in, std::string activate_function = "tanh")
-        : _weights(std::vector<Variable>(n_in)), _bias(0.0),
-          _activate_function(activate_function){};
+        : _weights(n_in), _activate_function(activate_function)
+    {
+        unsigned seed = static_cast<unsigned>(
+            std::chrono::system_clock::now().time_since_epoch().count());
+        std::default_random_engine generator(seed);
+        std::uniform_real_distribution<double> distribution(-1.0, 1.0);
+        distribution.reset();
+        for (size_t i = 0; i < n_in; i++)
+        {
+            // _weights[i] = Variable(1.0 * i, 0.0, "", "weights");
+            _weights[i] = Variable(distribution(generator), 0.0, "", "weights");
+            _weights[i].set_ref(&_weights[i]);
+        }
+        // _bias = Variable(1.0, 0.0, "", "bias");
+        _bias = Variable(distribution(generator), 0, "", "bias");
+        _bias.set_ref(&_bias);
+    }
 
     const std::vector<Variable> &weights() const
     {
