82397c793727009b4455b08904317108c5903393
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-16 15:17:43 +0800

    fix(layer.h): initialization error

--- src/layer/layer.h
+++ src/layer/layer.h
@@ -14,11 +14,20 @@ private:
 
 public:
     Layer(size_t n_in, size_t n_out, std::string activate_function = "tanh")
-        : _n_in(n_in), _n_out(n_out), _activate_function(activate_function),
-          _neurons(
-              std::vector<Neuron>(n_out, Neuron(n_in, activate_function))){};
+        : _n_in(n_in), _n_out(n_out), _activate_function(activate_function)
+    {
+        for (size_t i = 0; i < n_out; i++)
+        {
+            _neurons.push_back(Neuron(n_in, activate_function));
+        }
+    };
 
     std::vector<Variable> parameters() const;
+
+    const std::vector<Neuron> &neurons() const
+    {
+        return _neurons;
+    }
 
     std::vector<Variable> forward(const std::vector<double> &inputs);
     std::vector<Variable> forward(const std::vector<Variable> &variables);
