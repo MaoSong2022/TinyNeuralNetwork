1739e1a52f42459c2f97c35cae7ce6a4f810422a
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-17 17:29:39 +0800

    fix(layer.cc): fix reallocate error

--- src/layer/layer.cc
+++ src/layer/layer.cc
@@ -3,12 +3,14 @@
 
 std::vector<Variable> Layer::parameters() const
 {
-    std::vector<Variable> result;
-    for (const auto &neuron : _neurons)
+    std::vector<Variable> result(_n_out * (_n_in + 1));
+    for (size_t i = 0; i < _n_out; i++)
     {
-        result.insert(result.end(),
-                      neuron.parameters().begin(),
-                      neuron.parameters().end());
+        const auto &parameters = _neurons[i].parameters();
+        for (size_t j = 0; j < _n_in + 1; j++)
+        {
+            result[i * (_n_in + 1) + j] = parameters[j];
+        }
     }
     return result;
 }
