34f2e0f789b7f9b271d2922ce589aebb36ef87cc
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-17 18:45:38 +0800

    test(test_layer.cc): add test for variable inputs

--- tests/test_layer.cc
+++ tests/test_layer.cc
@@ -31,4 +31,45 @@ TEST_CASE("Test layer", "[Layer]")
                 Approx(child.gradient() * inputs[2]));
         REQUIRE(layer.parameters()[3].gradient() == Approx(child.gradient()));
     }
+
+    SECTION("Test variable inputs")
+    {
+        Layer layer(3, 1);
+        REQUIRE(layer.neurons().size() == 1);
+        REQUIRE(layer.neurons()[0].parameters().size() == 4);
+        REQUIRE(layer.parameters().size() == 4);
+
+        std::vector<Variable> inputs(3);
+        for (size_t i = 0; i < 3; ++i)
+        {
+            inputs[i] =
+                Variable(1.0 * i, 0.0, "", "input[" + std::to_string(i) + "]");
+        }
+        std::vector<Variable> results = layer.forward(inputs);
+        REQUIRE(results.size() == 1);
+        auto &result = results[0];
+        REQUIRE(result.children().size() == 1);
+        result.set_gradient(1.0);
+        result.backward();
+        REQUIRE(result.gradient() == Approx(1.0));
+
+        auto &child = result.children()[0];
+        REQUIRE(child.children().size() == 2);
+        REQUIRE(child.gradient() ==
+                Approx(1.0 - result.value() * result.value()));
+        REQUIRE(layer.parameters()[0].gradient() ==
+                Approx(child.gradient() * inputs[0].value()));
+        REQUIRE(layer.parameters()[1].gradient() ==
+                Approx(child.gradient() * inputs[1].value()));
+        REQUIRE(layer.parameters()[2].gradient() ==
+                Approx(child.gradient() * inputs[2].value()));
+        REQUIRE(layer.parameters()[3].gradient() == Approx(child.gradient()));
+
+        // input gradient check
+        for (size_t i = 0; i < 3; ++i)
+        {
+            REQUIRE(inputs[i].gradient() ==
+                    Approx(child.gradient() * layer.parameters()[i].value()));
+        }
+    }
 }
