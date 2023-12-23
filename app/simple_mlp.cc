e998f5e7437c22afec716bb24970b1d7cd14e417
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-23 20:53:20 +0800

    docs(simple_mlp.cc): add docs

--- app/simple_mlp.cc
+++ app/simple_mlp.cc
@@ -18,22 +18,26 @@ namespace fs = std::filesystem;
 
 int main()
 {
+    // traning data
     std::vector<double> inputs = {2.0, 3.0, -1.0};
     std::vector<double> targets = {1.0};
 
+    // construct mlp
     size_t num_inputs = inputs.size();
     std::vector<size_t> num_outputs{4, 4, 1};
-
     MLP mlp(num_inputs, num_outputs);
 
+    // configure training parameters
     size_t iters = 20;
     double lr = 0.01;
 
     for (size_t iter = 0; iter < iters; iter++)
     {
+        // forward pass
         const std::vector<Variable> &predictions = mlp.forward(inputs);
-        Variable loss = MSELoss(predictions, targets);
 
+        // compute and record loss
+        Variable loss = MSELoss(predictions, targets);
         spdlog::info(
             fmt::format("Iteration: {}. Loss: {}", iter, loss.value()));
 
@@ -43,6 +47,7 @@ int main()
             parameter.zero_grad();
         }
 
+        // set gradient in order to back propagate
         loss.set_gradient(1.0);
 
         // backward
