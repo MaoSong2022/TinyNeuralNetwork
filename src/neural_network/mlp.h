a516ab203fca668a0e3c6515111c16fdd94a4722
MaoSong2022 <maosong@pjlab.org.cn>
2023-12-23 17:05:43 +0800

    refactor(mlp.h): use _results field to avoid rvalue

--- src/neural_network/mlp.h
+++ src/neural_network/mlp.h
@@ -10,12 +10,14 @@ private:
     size_t _n_in;
     std::vector<size_t> _n_outs;
     std::vector<Layer> _layers;
+    std::vector<std::vector<Variable>> _results;
     std::vector<Variable> _parameters;
 
 public:
     MLP(size_t n_in, std::vector<size_t> n_outs) : _n_in(n_in), _n_outs(n_outs)
     {
         _layers.reserve(n_outs.size());
+        _results.resize(n_outs.size());
         size_t n_prev = n_in;
         size_t num_parameters = 0;
         for (size_t n_out : n_outs)
@@ -40,6 +42,10 @@ public:
         return _layers;
     }
 
+    const std::vector<std::vector<Variable>> &results() const
+    {
+        return _results;
+    }
 
     const std::vector<Variable> &parameters() const
     {
